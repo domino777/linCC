e2c36bbde05fa613bfcf272dec651dbef3933e7b
Walter Barbagallo <turbometalskater@gmail.com>
2014-03-14 01:12:24 +0100

    missing one indent :P

--- client/ajax.php
+++ client/ajax.php
@@ -4,65 +4,65 @@ require('functions.php');
 // TODO sicurezza? un api token?
 
 if (isset($_POST['action'])) {
-        switch ($_POST['action']) {
+    switch ($_POST['action']) {
 
-            case 'update':
-                isset($_POST['table'],
-                      $_POST['field'],
-                      $_POST['val'],
-                      $_POST['id']) or
-                    die("Malformed request. Check your form data");
+        case 'update':
+            isset($_POST['table'],
+                  $_POST['field'],
+                  $_POST['val'],
+                  $_POST['id']) or
+                die("Malformed request. Check your form data");
 
-                $link = new linCC_mysqli();
-                $link->table_update($_POST['table'],
-                                    $_POST['field'],
-                                    $_POST['val'],
-                                    $_POST['id']) or
-                    die("Something went wrong with the query");
-                $link->close();
-                echo "Update successful";
-                break;
-
-
-            case 'get_table':
-                isset($_POST['table']) or
-                    die("Malformed request. Check your form data");
-                get_table($_POST['table']);
-                break;
+            $link = new linCC_mysqli();
+            $link->table_update($_POST['table'],
+                                $_POST['field'],
+                                $_POST['val'],
+                                $_POST['id']) or
+                die("Something went wrong with the query");
+            $link->close();
+            echo "Update successful";
+            break;
 
 
-            case 'get':
-                isset($_POST['table'],
-                      $_POST['field']) or
-                    die("Malformed request. Check your form data");
-
-                $link = new linCC_mysqli();
-                $values = $link->get_col(
-                    $_POST['table'],
-                    $_POST['field']) or die("Something went wrong with the query");
-                $link->close();
-                echo json_encode($values);
-                break;
+        case 'get_table':
+            isset($_POST['table']) or
+                die("Malformed request. Check your form data");
+            get_table($_POST['table']);
+            break;
 
 
-            case 'remove':
-                isset($_POST['table'],
-                      $_POST['id']) or
-                    die("Malformed request. Check your form data");
+        case 'get':
+            isset($_POST['table'],
+                  $_POST['field']) or
+                die("Malformed request. Check your form data");
 
-                $link = new linCC_mysqli();
-                $values = $link->remove(
-                    $_POST['table'],
-                    $_POST['id']) or die("Something went wrong with the query");
-                $link->close();
-                echo "Remove successful";
-                break;
+            $link = new linCC_mysqli();
+            $values = $link->get_col(
+                $_POST['table'],
+                $_POST['field']) or die("Something went wrong with the query");
+            $link->close();
+            echo json_encode($values);
+            break;
 
 
-            default:
-                die("Action not defined");
-                break;
-        }
+        case 'remove':
+            isset($_POST['table'],
+                  $_POST['id']) or
+                die("Malformed request. Check your form data");
+
+            $link = new linCC_mysqli();
+            $values = $link->remove(
+                $_POST['table'],
+                $_POST['id']) or die("Something went wrong with the query");
+            $link->close();
+            echo "Remove successful";
+            break;
+
+
+        default:
+            die("Action not defined");
+            break;
+    }
 }
 else {
     die("You must specify an action as post form data!");
